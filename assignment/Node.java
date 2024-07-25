c172c888ee9135937c38f89eacce9c6306d37f48
hilla2702 <hillali2702@gmail.com>
2024-07-25 09:45:03 +0300

    COMMENDS TO NODE FILE

--- assignment/Node.java
+++ assignment/Node.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
-    private String name;
-    private List<Node> edges;
-    private Message message;
+    private String name;// The name of the node
+    private List<Node> edges;// The list of edges that go from node to others nodes
+    private Message message;// The masseges that this node can have
 
+    // Constractor, get the name of the node and init all the parameters
     public Node(String name) {
         this.name = name;
         this.edges = new ArrayList<>();
         this.message = null;
     }
 
+    // Get & Set of name
     public String getName() {
         return name;
     }
@@ -22,6 +24,7 @@ public class Node {
         this.name = name;
     }
 
+    // Get & Set of edges
     public List<Node> getEdges() {
         return edges;
     }
@@ -30,6 +33,7 @@ public class Node {
         this.edges = edges;
     }
 
+    // Get & Set of message
     public Message getMessage() {
         return message;
     }
@@ -38,15 +42,17 @@ public class Node {
         this.message = message;
     }
 
+    // Add edges to edges list
     public void addEdge(Node node) {
         this.edges.add(node);
     }
 
-    public boolean hasCycles() {
+    // Checking if the graph has cycles
+    public boolean hasCycles() {// the main method
         return hasCycles(new ArrayList<>());
     }
 
-    private boolean hasCycles(List<Node> visited) {
+    private boolean hasCycles(List<Node> visited) {// the auxiliary method, dfs
         if (visited.contains(this)) {
             return true;
         }
