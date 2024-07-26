96f694f975c95d0052d64d4e1d84cf53c996a092
hilla2702 <hillali2702@gmail.com>
2024-07-26 17:07:58 +0300

    incAgent update file

--- project/src/test/IncAgent.java
+++ project/src/test/IncAgent.java
@@ -1,8 +1,34 @@
 package test;
 
-import test.TopicManagerSingleton.TopicManager;
+//import test.TopicManagerSingleton.TopicManager;
 
-public class IncAgent {
+public class IncAgent implements Agent {
+    private String[] subs;
+    private String[] pubs;
 
-    
+    public IncAgent(String[] subs, String[] pubs) {
+        this.subs = subs;
+        this.pubs = pubs;
+        TopicManagerSingleton.get().getTopic(subs[0]).subscribe(this);
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        double value = msg.asDouble;
+        value++;
+        TopicManagerSingleton.get().getTopic(pubs[0]).publish(new Message(value));
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public String getName() {
+        return "IncAgent";
+    }
 }
